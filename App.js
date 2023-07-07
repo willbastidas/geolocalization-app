ed2b23347dc94bc114ed3f6fc684de74a834b164
willbastidas <willkergaciab@gmail.com>
2023-07-07 10:56:09 -0400

    ni idea men

--- App.js
+++ App.js
@@ -2,29 +2,30 @@ import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 // import { MapHTMLAttributes } from 'react';
-import MapView from 'react-native-maps';
+import MapView, {Marker} from 'react-native-maps';
 import {styles} from './styles/estilos';
 // import {local_default} from './components/localizacion_default'
 
 
 
 export default function App() {
-   const [origin, setOrigin] = React.useState ({
-      latitute: 10.49032,
-      longitude: -66.85351,
+   const [mapRegion, setMapRegion] = React.useState ({
+      lat: 10.49032,
+      long: -66.85351,
+      latDelta: 0.09,
+      longDelta: 0.04,
    });
 
   return (
     <View style={styles.container}>
         <MapView style={styles.map}
           initialRegion={{
-             latitude: origin.latitude,
-            longitude: origin.longitude,
-             latitudeDelta: 0.09,
-             longitudeDelta: 0.04,
-           }}
-         >
-          
+            latitude: 10.49032,
+            longitude: -66.85351, 
+            tatitudeDelta: 0.09,
+            longitudeDelta: 0.04,
+          }}
+         > 
          </MapView>
     </View>
   );
